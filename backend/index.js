a2efc399b00249c45cb91483c0ea56290ebd6ab6
Aravindh0799 <aravindhan0799@gmail.com>
2024-06-01 19:12:57 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,15 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin:"*",
+    origin: "*",
 }))
 
 const routes = require('./routes/route')
 app.use(routes)
 
-const port = process.env.port || 8080;
+const port = process.env.port || 10080;
 require('./conn')
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server running at port ${port} `);
 })
